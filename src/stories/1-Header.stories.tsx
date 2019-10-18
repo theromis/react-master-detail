1d67cf20c221bf57c024464a74a2cd83a694fccd
Martin Pritchard <martinpritchard@elevatedirect.com>
2019-10-18 02:32:45 +0100

    Setup Storybook

---
+++ src/stories/1-Header.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
+import { Header } from '../components';
+
+export default {
+  title: 'Header',
+};
+
+storiesOf('Header', module)
+  .addDecorator(story => (
+    <Router><Route path="/" >{ story() }</Route></Router>
+  ))
+  .add('NoTitle', () => <Header />); 
+
