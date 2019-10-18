d549e745ee412b5116921cc7532e0c8755039078
Martin Pritchard <martinpritchard@elevatedirect.com>
2019-10-18 11:25:27 +0100

    Storybook > Add & configure `@storybook/addon-viewport`
    For responive testing

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,10 @@
 import '../src/index.scss';
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 // automatically import all files ending in *.stories.js
+addParameters({
+    viewport: {
+        viewports: INITIAL_VIEWPORTS,
+    },
+});
 configure(require.context('../src/stories', true, /\.stories\.tsx$/), module);
