5aeb0675cfc9458f6c1d2275bf3478feb3857ff3
Martin Pritchard <martinpritchard@elevatedirect.com>
2019-10-21 18:43:07 +0100

    Misc minor tidy up

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import 'index.scss';
 import { addParameters, configure } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
-// automatically import all files ending in *.stories.js
+
 addParameters({
     viewport: {
         viewports: INITIAL_VIEWPORTS,
