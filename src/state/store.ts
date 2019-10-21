9246911d0ecd88fc62fc4783d3c9382177b17770
Martin Pritchard <martinpritchard@elevatedirect.com>
2019-10-21 12:51:17 +0100

    Add redux devtools support

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,13 @@
 import { combineReducers, createStore } from 'redux';
 import { example } from './example.reducer';
 import { ExampleState } from './example.model';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export interface AppStore {
     example: ExampleState
 }
 
-export const store = createStore(combineReducers({ example: example }));
+export const store = createStore(
+    combineReducers({ example: example }),
+    composeWithDevTools() 
+);
