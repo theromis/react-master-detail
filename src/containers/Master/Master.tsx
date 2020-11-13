54e1d9bf96518d84a84a6c6debaac5b242e65026
Roman Vasilyev <roman@lonje.com>
2020-11-12 20:29:52 -0800

    load more

--- src/containers/Master/Master.tsx
+++ src/containers/Master/Master.tsx
@@ -4,6 +4,7 @@ import { RouteProps } from 'react-router-dom';
 import { useRouteMatch } from 'react-router-dom';
 import { Header, ListItemLink } from 'components';
 import { Items, Item, getExampleItems, AppStore } from 'state';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export interface MasterProps extends Items {
 }
@@ -11,6 +12,9 @@ export interface MasterProps extends Items {
 export const MasterContainer: React.FC<MasterProps> = (props) => {
     let { path } = useRouteMatch() as RouteProps;
     let { items } = useSelector((store: AppStore) => getExampleItems(store));
+    const fetchData = () => {
+        console.log("Load more masters ...")
+    }
     const listItems = items.map((item: Item) =>
         <li key={item.id}>
             <ListItemLink 
@@ -21,7 +25,14 @@ export const MasterContainer: React.FC<MasterProps> = (props) => {
         <React.Fragment>
             <Header title="Master" hideBackButton={true}/>
             <ul>
+            <InfiniteScroll
+  dataLength={listItems.length}
+  next={fetchData}
+  hasMore={true}
+  loader={<h4>Loading...</h4>}
+  >
                 {listItems}
+                </InfiniteScroll>
             </ul>
         </React.Fragment>
     );
